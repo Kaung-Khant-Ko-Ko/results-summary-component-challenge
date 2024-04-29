e9a15b5cdd3c1903ac0355b4028ac0da1e97990c
Kaung Khant Ko Ko <59255928+Kaung-Khant-Ko-Ko@users.noreply.github.com>
2024-04-29 21:03:48 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+fetch("./data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    document.querySelector("#react-icon").setAttribute("src", data[0].icon);
+    document.querySelector("#react-text").textContent = data[0].category;
+    document.querySelector("#react-score").innerHTML =
+      data[0].score + "<span class='total'> / 100</span>";
+
+    document.querySelector("#memo-icon").setAttribute("src", data[1].icon);
+    document.querySelector("#memo-text").textContent = data[1].category;
+    document.querySelector("#memo-score").innerHTML =
+      data[1].score + "<span class='total'> / 100</span>";
+
+    document.querySelector("#verb-icon").setAttribute("src", data[2].icon);
+    document.querySelector("#verb-text").textContent = data[2].category;
+    document.querySelector("#verb-score").innerHTML =
+      data[2].score + "<span class='total'> / 100</span>";
+
+    document.querySelector("#vis-icon").setAttribute("src", data[3].icon);
+    document.querySelector("#vis-text").textContent = data[3].category;
+    document.querySelector("#vis-score").innerHTML =
+      data[3].score + "<span class='total'> / 100</span>";
+  });
